ceb280b948789c9b17ae17ff0a49c83a067810e3
David Kerr <kerrky@gmail.com>
2024-03-29 14:10:53 -0400

    Complete lab

--- server/app.py
+++ server/app.py
@@ -2,4 +2,20 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+  return f"<h1>Profile for {username}</h1>"
+
+@app.route('/<string:favorite>')
+def favorite(fav):
+   return f"<h2>This is my {fav} page!</h2>"
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
